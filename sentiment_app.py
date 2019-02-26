184726f28c44ec991c3ba20f730726ebd25ae4ea
yeagster <35350657+yeagster@users.noreply.github.com>
2019-02-25 21:54:57 -0700

    Updated app:app

--- sentiment_app.py
+++ sentiment_app.py
@@ -24,7 +24,7 @@ from datetime import datetime as dt
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
+# server = app.server
 
 styles = {
     'pre': {
